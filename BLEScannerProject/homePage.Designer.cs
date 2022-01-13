3bb3266882bdfe9d28f910789c666f16755fbc5b
Aylin Kar <aylinkar97@gmail.com>
2022-01-13 17:49:18 +0300

    Add project files.

---
+++ BLEScannerProject/homePage.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace BLEScanner
+{
+    partial class homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblDistrict = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtInsertsUpdates = new System.Windows.Forms.TextBox();
+            this.txtDistrict = new System.Windows.Forms.TextBox();
+            this.btnAddDistrict = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmxDistricts = new System.Windows.Forms.ComboBox();
+            this.dgDevices = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDevices)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(888, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Finish Time:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(952, 61);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(888, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Start Time:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(952, 32);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 37;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Device:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(735, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 35;
+            // 
+            // lblDistrict
+            // 
+            this.lblDistrict.AutoSize = true;
+            this.lblDistrict.Location = new System.Drawing.Point(500, 35);
+            this.lblDistrict.Name = "lblDistrict";
+            this.lblDistrict.Size = new System.Drawing.Size(42, 13);
+            this.lblDistrict.TabIndex = 34;
+            this.lblDistrict.Text = "District:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(548, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 33;
+            // 
+            // txtInsertsUpdates
+            // 
+            this.txtInsertsUpdates.Enabled = false;
+            this.txtInsertsUpdates.Location = new System.Drawing.Point(8, 267);
+            this.txtInsertsUpdates.Multiline = true;
+            this.txtInsertsUpdates.Name = "txtInsertsUpdates";
+            this.txtInsertsUpdates.ReadOnly = true;
+            this.txtInsertsUpdates.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInsertsUpdates.Size = new System.Drawing.Size(234, 118);
+            this.txtInsertsUpdates.TabIndex = 32;
+            // 
+            // txtDistrict
+            // 
+            this.txtDistrict.Location = new System.Drawing.Point(11, 391);
+            this.txtDistrict.Name = "txtDistrict";
+            this.txtDistrict.Size = new System.Drawing.Size(231, 20);
+            this.txtDistrict.TabIndex = 31;
+            // 
+            // btnAddDistrict
+            // 
+            this.btnAddDistrict.Location = new System.Drawing.Point(8, 417);
+            this.btnAddDistrict.Name = "btnAddDistrict";
+            this.btnAddDistrict.Size = new System.Drawing.Size(234, 49);
+            this.btnAddDistrict.TabIndex = 30;
+            this.btnAddDistrict.Text = "ADD DISTRICT";
+            this.btnAddDistrict.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "SELECT DISTRICT:";
+            // 
+            // cmxDistricts
+            // 
+            this.cmxDistricts.FormattingEnabled = true;
+            this.cmxDistricts.Location = new System.Drawing.Point(8, 118);
+            this.cmxDistricts.Name = "cmxDistricts";
+            this.cmxDistricts.Size = new System.Drawing.Size(234, 21);
+            this.cmxDistricts.TabIndex = 27;
+            // 
+            // dgDevices
+            // 
+            this.dgDevices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDevices.Location = new System.Drawing.Point(503, 93);
+            this.dgDevices.Name = "dgDevices";
+            this.dgDevices.Size = new System.Drawing.Size(762, 471);
+            this.dgDevices.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Runtime:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 211);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 36);
+            this.textBox2.TabIndex = 25;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(128, 145);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(114, 60);
+            this.btnStop.TabIndex = 24;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(8, 145);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(114, 60);
+            this.btnStart.TabIndex = 23;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(8, 9);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(234, 65);
+            this.btnScan.TabIndex = 22;
+            this.btnScan.Text = "REFRESH";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 582);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblDistrict);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtInsertsUpdates);
+            this.Controls.Add(this.txtDistrict);
+            this.Controls.Add(this.btnAddDistrict);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmxDistricts);
+            this.Controls.Add(this.dgDevices);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnScan);
+            this.Name = "homepage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgDevices)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblDistrict;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtInsertsUpdates;
+        private System.Windows.Forms.TextBox txtDistrict;
+        private System.Windows.Forms.Button btnAddDistrict;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmxDistricts;
+        private System.Windows.Forms.DataGridView dgDevices;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
---
+++ BLEScannerProject/homePage.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using InTheHand.Net;
+using System.IO;
+
+namespace BLEScanner
+{
+    public partial class homepage : Form
+    {
+        public homepage()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=JARVIS;Initial Catalog=BLEScannerDB;Integrated Security=True;MultipleActiveResultSets=true");
+        int sayac = 0;
+        int presentScanID = 0;
+        string districtOfScan = "";
+        string districtName = "";
+        string currentDateTime = "";
+        string previousDateTime = "";
+        string insertOrUpdateState = "";
+        int startState = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+            timer1.Interval = 1000;
+            BindGrid();
+
+            SqlDataReader dr;
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "SELECT Name FROM Districts";
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cmxDistricts.Items.Add(dr["Name"]); ;
+            }
+            conn.Close();
+            dr.Close();
+        }
+
+        async void ScanForMSSQL()
+        {
+            SqlDataReader dr;
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            currentDateTime = DateTime.Now.ToString();
+
+            cmd.CommandText = "SELECT * FROM Devices";
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows) startState = 1;
+            else startState = 0;
+            conn.Close();
+            dr.Close();
+            cmd.Parameters.Clear();
+
+            if (startState == 0)
+            {
+                cmd.CommandText = "INSERT INTO Devices (ScanID, Name, Address, District, CheckIn, CheckOut) " +
+                         "values (@scanID, @name, @address, @district, @checkIn, @checkOut)";
+                cmd.Parameters.AddWithValue("@scanID", 0);
+                cmd.Parameters.AddWithValue("@name", "Name");
+                cmd.Parameters.AddWithValue("@address", "Address");
+                cmd.Parameters.AddWithValue("@district", districtOfScan);
+                cmd.Parameters.AddWithValue("@checkIn", currentDateTime);
+                cmd.Parameters.AddWithValue("@checkOut", currentDateTime);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                cmd.Parameters.Clear();
+            }
+
+            cmd.CommandText = "SELECT MAX(ScanID) AS ScanIDcounter, MAX(CheckOut) AS PreviousCheckOut FROM Devices";
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                presentScanID = Convert.ToInt32((dr["ScanIDcounter"]).ToString());
+                previousDateTime = (dr["PreviousCheckOut"]).ToString();
+            }
+            conn.Close();
+            dr.Close();
+
+            presentScanID += 1;
+
+            BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
+            BluetoothClient client = new BluetoothClient();
+            BluetoothDeviceInfo[] devices = client.DiscoverDevices();
+            BluetoothClient bluetoothClient = new BluetoothClient();
+
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                cmd.CommandText = "SELECT * FROM Devices WHERE Name = @name AND Address = @address AND District = @district AND Checkout = @previousTime";
+                cmd.Parameters.AddWithValue("@name", device.DeviceName.ToString());
+                cmd.Parameters.AddWithValue("@address", device.DeviceAddress.ToString());
+                cmd.Parameters.AddWithValue("@district", districtOfScan);
+                cmd.Parameters.AddWithValue("@previousTime", previousDateTime);
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows) insertOrUpdateState = "update";
+                else insertOrUpdateState = "insert";
+                txtInsertsUpdates.Invoke((MethodInvoker)(() => txtInsertsUpdates.Text = Environment.NewLine + device.DeviceName.ToString() + " " +
+                    device.DeviceAddress.ToString() + " " + insertOrUpdateState + Environment.NewLine + txtInsertsUpdates.Text));
+                conn.Close();
+                dr.Close();
+                cmd.Parameters.Clear();
+
+                if (insertOrUpdateState == "insert")
+                {
+                    cmd.CommandText = "INSERT INTO Devices (ScanID, Name, Address, District, CheckIn, CheckOut) " +
+                         "values (@scanID, @name, @address, @district, @checkIn, @checkOut)";
+                    cmd.Parameters.AddWithValue("@scanID", presentScanID);
+                    cmd.Parameters.AddWithValue("@name", device.DeviceName.ToString());
+                    cmd.Parameters.AddWithValue("@address", device.DeviceAddress.ToString());
+                    cmd.Parameters.AddWithValue("@district", districtOfScan);
+                    cmd.Parameters.AddWithValue("@checkIn", currentDateTime);
+                    cmd.Parameters.AddWithValue("@checkOut", currentDateTime);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    cmd.Parameters.Clear();
+                }
+                else if (insertOrUpdateState == "update")
+                {
+                    //update tEmployees set ePassword=@password where eUserName=@username
+                    cmd.CommandText = "UPDATE Devices SET CheckOut = @checkOut WHERE Name = @name AND Address = @address" +
+                        " AND District = @district AND CheckOut = @previousTime";
+                    cmd.Parameters.AddWithValue("@name", device.DeviceName.ToString());
+                    cmd.Parameters.AddWithValue("@address", device.DeviceAddress.ToString());
+                    cmd.Parameters.AddWithValue("@district", districtOfScan);
+                    cmd.Parameters.AddWithValue("@previousTime", previousDateTime);
+                    cmd.Parameters.AddWithValue("@checkOut", currentDateTime);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    cmd.Parameters.Clear();
+                }
+            }
+        }
+
+        private void BindGrid()
+        {
+            SqlDataReader dr;
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "SELECT ScanID, ID, Name, Address, District, CheckIn, CheckOut FROM Devices";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            conn.Open();
+            da.Fill(ds, "Devices");
+            dgDevices.DataSource = ds.Tables["Devices"];
+            conn.Close();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //if (backgroundWorker1.IsBusy != true) {
+            //    backgroundWorker1.RunWorkerAsync();
+            //}
+            SqlDataReader dr;
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            if (cmxDistricts.SelectedItem != null)
+            {
+                cmd.CommandText = "SELECT Name FROM Districts WHERE Name = @name";
+                cmd.Parameters.AddWithValue("@name", cmxDistricts.SelectedItem.ToString());
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    districtOfScan = (dr["Name"]).ToString(); /*Convert.ToInt32*/
+                }
+                conn.Close();
+                dr.Close();
+                MessageBox.Show(cmxDistricts.SelectedItem.ToString() + " için tarama başlatıldı!");
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please select a district to start scanning!");
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //if (backgroundWorker1.WorkerSupportsCancellation == true)
+            //{
+            //    // Cancel the asynchronous operation.
+            //    backgroundWorker1.CancelAsync();
+            //}
+            timer1.Stop();
+            backgroundWorker1.CancelAsync();
+            sayac = 0;
+            textBox2.Text = sayac.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            textBox2.Text = sayac.ToString();
+            if (backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ScanForMSSQL();
+        }
+
+        private void dgDevices_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnAddDistrict_Click(object sender, EventArgs e)
+        {
+            SqlDataReader dr;
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "SELECT districtName FROM tDistricts WHERE districtName = @name";
+            cmd.Parameters.AddWithValue("@name", txtDistrict.Text);
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                districtName = (dr["districtName"]).ToString();
+            }
+            conn.Close();
+            dr.Close();
+            cmd.Parameters.Clear();
+
+            if (districtName == "")
+            {
+                cmd.CommandText = "INSERT INTO tDistricts (districtName) values (@name)";
+                cmd.Parameters.AddWithValue("@name", txtDistrict.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                cmd.Parameters.Clear();
+                MessageBox.Show("District created!");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a district name that has not been created before!");
+            }
+
+        }
+    }
+}
+
